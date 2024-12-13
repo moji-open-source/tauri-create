5a7ce102695973969f0d28c6d83c8636952d8acf
Clover <2621869236@qq.com>
2024-12-13 12:08:38 +0800

    feat: print cli version

--- src/runner.ts
+++ src/runner.ts
@@ -1,5 +1,14 @@
+import colors from 'picocolors'
+
+import packageJson from '../package.json'
+
 export type Runner = () => Promise<void>
 
+const { green, red } = colors
 export async function runCli(task: Runner) {
+  console.log(`✨ ${green('Moji Tauri CLI')}: ${red(`v${packageJson.version}`)}`)
+
+  console.log()
+
   await task()
 }
